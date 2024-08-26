67501ff38ce6e442fff92d35f4e805b492aa6a21
Cesar Sousa <4893784+cesarssa@users.noreply.github.com>
2024-08-26 20:53:03 -0300

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', function(req, res, next) {
+  const formData = {
+    fields: [
+      {
+        field: {
+          label: 'Name',
+          type: 'text',
+          name: 'name'
+        }
+      },
+      {
+        field: {
+          label: 'Email',
+          type: 'email',
+          name: 'email'
+        }
+      },
+      {
+        field: {
+          label: 'Password',
+          type: 'password',
+          name: 'password'
+        }
+      }
+    ]
+  };
+  
+  res.render('form', formData);
+});
+
+module.exports = router;
